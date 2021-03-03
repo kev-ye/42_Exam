2c145f230ffb28cf5e81f6372a2d2bf51b560f4b
Kangkai YE <kaye@e1r9p16.42.fr>
2021-03-03 12:28:07 +0100

    exam

---
+++ 42_Exam_rank_03/micro_paint.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct  s_bg
+{
+    int     width;
+    int     height;
+    char    c; 
+}               t_bg;
+
+typedef struct  s_draw
+{
+    char    c;
+    float   x;
+    float   y; 
+    float   width;
+    float   height;
+    char    cc;
+}               t_draw;
+
+static int     msg_error(const char *msg, int ret)
+{
+    printf("%s\n", msg);
+    return (ret);
+}
+
+static int check_zone(FILE *file)
+{
+    int     get;
+    t_bg    *zone;
+
+    get = 0;
+    memset(zone, 0, sizeof(t_bg));
+    if ((get = fscanf(file, "%d %d %c\n", zone->width, zone->height, zone->c) == 3))
+    {
+        if (!(zone->width > 0 && zone->width <= 300) || !(zone->height > 0 && zone->height <= 300))
+            return (0);
+        if (zone->c <= 0)
+            return (0);
+        return (1);
+    }
+    fclose(file);
+    return (0);
+}
+
+static int check_draw(FILE *file)
+{
+    int     get;
+    t_draw  *draw;
+
+    get = 0;
+    memset(draw, 0, sizeof(t_draw));
+    // while ()
+    // {
+        
+    // }
+}
+
+int main(int ac, char **av)
+{
+    FILE *file;
+
+    if (ac != 2)
+        return(msg_error("Error: argument" ,1));
+    if (!(file = fopen(av[1], "r")))
+        return(msg_error("Error: Operation file corrupted" ,1));
+    if (!(check_zone))
+        return(msg_error("Error: Operation file corrupted" ,1));
+    return (0);
+}
