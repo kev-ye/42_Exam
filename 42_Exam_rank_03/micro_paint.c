abe8d0f83bd5bca669db22e33a7fa25f251fa8bd
Kangkai YE <kaye@e1r9p16.42.fr>
2021-03-03 13:52:36 +0100

    exam

--- 42_Exam_rank_03/micro_paint.c
+++ 42_Exam_rank_03/micro_paint.c
@@ -30,30 +30,28 @@ static int check_zone(FILE *file)
     t_bg    zone;
 
     get = 0;
-    if ((get = fscanf(file, "%d %d %c\n", &zone.width, &zone.height, &zone.c) == 3))
-    {
-        if (!(zone.width > 0 && zone.width <= 300) || !(zone.height > 0 && zone.height <= 300))
-            return (0);
-        if (zone.c <= 0)
-            return (0);
-        printf("%d\n", get);
-        return (1);
-    }
-    return (0);
+    memset(&zone, 0, sizeof(t_bg));
+    if ((get = fscanf(file, "%d %d %c\n", &zone.width, &zone.height, &zone.c)) != 3)
+        return (0);
+    if (!(zone.width > 0 && zone.width <= 300) || !(zone.height > 0 && zone.height <= 300))
+        return (0);
+    if (get == -1)
+        return (0);
+    return (1);
 }
 
-// static int check_draw(FILE *file)
-// {
-//     int     get;
-//     t_draw  *draw;
+static int check_draw(FILE *file)
+{
+    int     get;
+    t_draw  draw;
 
-//     get = 0;
-//     memset(draw, 0, sizeof(t_draw));
-//     // while ()
-//     // {
-        
-//     // }
-// }
+    get = 0;
+    memset(&draw, 0, sizeof(t_draw));
+    while (get = fscanf(file, "%c %f %f %f %f %c\n", &draw.c, &draw.x, &draw.y, &draw.width, &draw.height, &draw.cc)) == 6)
+    {
+       
+    }
+}
 
 int main(int ac, char **av)
 {
