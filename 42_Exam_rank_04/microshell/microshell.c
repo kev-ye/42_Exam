11845a08eeb9930fb18ff448ab22e77b9e95c63e
Kangkai YE <kaye@e2r7p4.42.fr>
2021-07-18 18:42:59 +0200

    progressing

--- 42_Exam_rank_04/microshell/microshell.c
+++ 42_Exam_rank_04/microshell/microshell.c
@@ -6,7 +6,7 @@
 /*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/13 17:18:05 by kaye              #+#    #+#             */
-/*   Updated: 2021/07/18 16:52:04 by kaye             ###   ########.fr       */
+/*   Updated: 2021/07/18 17:28:44 by kaye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@
 #include <signal.h>
 
 #define E_CD_ARG "error: cd: bad arguments\n"
-#define E_CD_FAIL "error: cd: cannot change directory to"
+#define E_CD_FAIL "error: cd: cannot change directory to "
+#define E_EXEC "error: cannot execute "
 #define E_FATAL "error: fatal\n"
 
 #define SUCCESS 0
@@ -35,8 +36,9 @@ typedef struct s_microshell
     int     flag;
 }   t_microshell;
 
-extern char **environ;
-t_microshell microshell;
+extern char     **environ;
+t_microshell    microshell;
+int             fd[2];
 
 void    _b_zero(void *ptr, size_t size)
 {
@@ -118,19 +120,36 @@ void    _free_args(void)
     }
 }
 
-void    __exit__(char *msg1, char *msg2, int fd, int ret)
+void    __msg_print__(char *msg1, char *msg2, int fd)
 {
-    if (msg1)
+   if (msg1)
         _putstr_fd(msg1, fd);
     if (msg2)
+    {
         _putstr_fd(msg2, fd);
+        _putstr_fd("\n", fd);
+    }
+}
+
+void    __exit__(char *msg1, char *msg2, int fd, int ret)
+{
+    __msg_print__(msg1, msg2, fd);
     _free_args();
     exit(ret);
 }
 
 void    _cd(void)
 {
-    
+    if (microshell.len != 2)
+    {
+        __msg_print__(E_CD_ARG, NULL, STDERR_FILENO);
+        return ;
+    }
+    if (chdir(microshell.args[1]) == -1)
+    {
+        __msg_print__(E_CD_FAIL, microshell.args[1], STDERR_FILENO);
+        return ;
+    }
 }
 
 void    _parser(char **av, int i)
@@ -175,19 +194,40 @@ void    _fork_exec(void)
         else if (pid == 0)
         {
             if (execve(microshell.args[0], microshell.args, environ) == -1)
-                __exit__(NULL, NULL, STDERR_FILENO, FAILURE);
+                __exit__(E_EXEC, microshell.args[0], STDERR_FILENO, FAILURE);
         }
         else
-        {
             waitpid(pid, NULL, 0);
-        }
+    }
+}
+
+void    _fork_exec_pipe(void)
+{
+    pid_t pid;
+
+    pipe(fd);
+    pid = fork();
+    if (pid < 0)
+        __exit__(E_FATAL, NULL, STDERR_FILENO, FAILURE);
+    else if (pid == 0)
+    {
+        close(fd[0]);
+        dup2(fd[1], STDOUT_FILENO);
+        if (execve(microshell.args[0], microshell.args, environ) == -1)
+            __exit__(E_EXEC, microshell.args[0], STDERR_FILENO, FAILURE);
+    }
+    else
+    {
+        close(fd[1]);
+        dup2(fd[0], STDIN_FILENO);
+        waitpid(pid, NULL, 0);
     }
 }
 
 void    _exec(char **av)
 {
-    size_t i;
-    int j;
+    size_t  i;
+    int     j;
 
     i = 1;
     while (av[i])
@@ -195,6 +235,8 @@ void    _exec(char **av)
         _parser(av, i);
         if (microshell.flag == NO_FLAG || microshell.flag == MULTI)
             _fork_exec();
+        else if (microshell.flag == PIPE)
+            _fork_exec_pipe();
         if (microshell.flag == NO_FLAG)
             i += microshell.len;
         else
@@ -208,6 +250,8 @@ int main(int ac, char **av)
 {
     if (ac <= 1)
         return (0);
+    fd[0] = -1;
+    fd[1] = -1;
     _exec(av);
     return (0);
 }
